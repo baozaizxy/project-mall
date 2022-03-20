bcbdc087a37d0be16eaaed74221b7166979f15f4
sherry-Zheng <674868933@qq.com>
2022-03-20 15:15:25 +0800

    项目目录结构设置

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    
+  },
   data(){
-    retuen{
-    age 30;
+    return{
     }
+  },
+  mounted(){
+    
   }
 }
 </script>
