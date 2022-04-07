0b0ed3c0dc0765a14a370e9a75e1cf04ea9a7c21
sherry-Zheng <674868933@qq.com>
2022-04-07 14:32:32 +0800

    debug

--- src/App.vue
+++ src/App.vue
@@ -24,9 +24,9 @@ export default {
 @import'./assets/scss/reset.scss'
 </style>
 
-<script type="text/javascript"> function load(){
-  var ele = document.getElementsByTagName("div")[0];  
-var content = ele.innerHTML;  console.log(content);
- } 
-window.onload = load;
-</script>
+<script type="text/javascript"> function load(){ 
+  var ele = document.getElementsByTagName("div")[0]; 
+  var content = ele.innerHTML; 
+  console.log(content); } 
+  window.onload = load;
+  </script>
